d504525f380b0dec5f6debff845a4e87e4356549
amanwarlock <amanwarlock@rocketmail.com>
2020-02-21 21:57:03 -0800

    2x1 implementation

--- 16x1_Multiplexer/structural/mux_2x1.v
+++ 16x1_Multiplexer/structural/mux_2x1.v
@@ -9,7 +9,12 @@ module mux_2x1 #(parameter DATA_WIDTH=2)(
 			input sel,
 			output out
 		);
+	wire t1,t2,t3;
+	//assign out = a_in[sel];
 
-	assign out = a_in[sel];
+	not g1(t1,sel);
+	and g2(t2,a_in[0],t1);
+	and g3(t3,a_in[1],sel);
+	or g4(out,t2,t3);
 
 endmodule
