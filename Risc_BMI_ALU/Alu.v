630fb09e547d235135a661d5c7819f3f444f123a
unknown <aman@TSINNOVATE-12.ad.csufresno.edu>
2020-03-30 02:24:44 -0700

    256 bits parity generator

--- Risc_BMI_ALU/Alu.v
+++ Risc_BMI_ALU/Alu.v
@@ -10,7 +10,28 @@ module Alu #(
 	input [1:0] opcode,
 	input [DATA_WIDTH-1: 0] A_in,
 	input [DATA_WIDTH-1: 0] B_in,
-	input [DATA_WIDTH-1: 0] Alu_out
+	output[DATA_WIDTH-1: 0] Alu_out
 );
+
+parameter PARITY = 2'b00, ROTR = 2'b01, ROTL = 2'b10, POPCOUNT = 2'b11;
+
+wire [DATA_WIDTH-1:0] result;
+
+reg parity_enable;
+
+reg [DATA_WIDTH-1:0] parity_in;
+
+always @(clk,opcode) begin 
+	case(opcode)
+		PARITY: begin
+			parity_enable = 1'b1;
+			parity_in = A_in;
+		end
+	endcase
+end
+
+Parity #(.DATA_WIDTH(DATA_WIDTH)) parity(.clk(clk), .enable(parity_enable), .p_in(parity_in), .p_out(result));
+
+assign Alu_out = result;
 
 endmodule
